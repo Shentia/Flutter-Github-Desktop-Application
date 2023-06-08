be86132c6de2b8a8e96d7296b07cbeeca7b2b68c
Ahmadreza Shamimi <ahmadreza.shamimi@gmail.com>
2023-06-08 00:15:46 -0400

    Update github_oauth_credentials.dart
    Delete the Credentials

--- gitloginauth/lib/github_oauth_credentials.dart
+++ gitloginauth/lib/github_oauth_credentials.dart
@@ -1,6 +1,6 @@
 // TODO(CodelabUser): Create an OAuth App
-const githubClientId = '4a343fa9a4e3bf4eab3d';
-const githubClientSecret = 'fc3141f033276624e8caceda967fd49acd8dfd4f';
+const githubClientId = 'YOUR GITHUBCLIENTID';
+const githubClientSecret = 'YOUR GITHUB CLIENT SECRET' ;
 
 // OAuth scopes for repository and user information
 const githubScopes = ['repo', 'read:org'];
